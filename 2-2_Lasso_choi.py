3e35e1d812ec461dee639875d0e8e68b4f6e3345
yoonsuk <yoonsuk.choi93@gmail.com>
2023-11-04 17:11:41 +0900

    testcommit

--- 2-2_Lasso_choi.py
+++ 2-2_Lasso_choi.py
@@ -1,1 +1,5 @@
-#
+#준비하기
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LassoCV, Lasso
